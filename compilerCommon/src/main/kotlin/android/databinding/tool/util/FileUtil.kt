6bd8059056641cd89d0c03b2e75cb23a85ce7a99
Hung Nguyen <hungnv@google.com>
2021-05-12 15:13:53 +0100

    Use Unix-like paths when sorting files
    so that the returned result is stable across OS-es.
    
    Bug: 187340555
    Test: Existing tests
    Change-Id: I58b893787dcb7bc450ddcbffd9377d3ef239d822

--- compilerCommon/src/main/kotlin/android/databinding/tool/util/FileUtil.kt
+++ compilerCommon/src/main/kotlin/android/databinding/tool/util/FileUtil.kt
@@ -24,28 +24,31 @@ import java.io.File
 object FileUtil {
 
     /**
-     * Returns files in a directory, sorted by path.
+     * Returns files in a directory, sorted by [File.invariantSeparatorsPath], so that the returned
+     * result is stable across OS-es.
      *
      * @see org.apache.commons.io.FileUtils.listFiles(java.io.File, org.apache.commons.io.filefilter.IOFileFilter, org.apache.commons.io.filefilter.IOFileFilter)
      */
     @JvmStatic
     fun listAndSortFiles(
-        directory: File,
-        fileFilter: IOFileFilter = TrueFileFilter.TRUE,
-        dirFilter: IOFileFilter = TrueFileFilter.TRUE
+            directory: File,
+            fileFilter: IOFileFilter = TrueFileFilter.TRUE,
+            dirFilter: IOFileFilter = TrueFileFilter.TRUE
     ): List<File> {
-        return FileUtils.listFiles(directory, fileFilter, dirFilter).sortedBy { it.path }
+        return FileUtils.listFiles(directory, fileFilter, dirFilter)
+                .sortedBy { it.invariantSeparatorsPath }
     }
 
     /**
-     * Returns files in a directory, sorted by path.
+     * Returns files in a directory, sorted by [File.invariantSeparatorsPath], so that the returned
+     * result is stable across OS-es.
      *
      * @see org.apache.commons.io.FileUtils.listFiles(java.io.File, java.lang.String[], boolean)
      */
     @JvmStatic
-    fun listAndSortFiles(
-        directory: File, extensions: Array<String>, recursive: Boolean
-    ): List<File> {
-        return FileUtils.listFiles(directory, extensions, recursive).sortedBy { it.path }
+    fun listAndSortFiles(directory: File, extensions: Array<String>, recursive: Boolean)
+            : List<File> {
+        return FileUtils.listFiles(directory, extensions, recursive)
+                .sortedBy { it.invariantSeparatorsPath }
     }
 }
