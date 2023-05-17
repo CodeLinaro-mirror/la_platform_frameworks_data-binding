120a1fb1128ce7bbdd0479dfbc3502fc5fa31fb4
Esteban de la Canal <estebandlc@google.com>
2023-05-17 16:50:43 -0700

    Make test suites remove themselves
    ... instead of having each one do it.
    
    Test: this is for tests
    Bug: NA
    Change-Id: Ide8510834f66f14bc9291dfe4402a2808ffd7ae4

--- compilationTests/src/test/java/androidx/databinding/compilationTest/bazel/DataBindingCompilationTestSuite.kt
+++ compilationTests/src/test/java/androidx/databinding/compilationTest/bazel/DataBindingCompilationTestSuite.kt
@@ -20,7 +20,6 @@ import com.android.tools.tests.IdeaTestSuiteBase
 import org.junit.runner.RunWith
 
 @RunWith(JarTestSuiteRunner::class)
-@JarTestSuiteRunner.ExcludeClasses(DataBindingCompilationTestSuite::class)
 class DataBindingCompilationTestSuite: IdeaTestSuiteBase() {
     companion object {
         init {
